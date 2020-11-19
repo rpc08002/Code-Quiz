9a6ad414815b4aa329269942f2be8d82e5fa3053
rpc08002 <rcurtin0491@gmail.com>
2020-11-19 17:23:07 -0500

    added question array with choices, questions made up for time being

--- questions.js
+++ questions.js
@@ -3,3 +3,44 @@ const choices = Array.from(document.querySelectorAll('.choice-text'));
 const progressText = document.querySelector('#progressText');
 const scoreText = document.querySelector('#score');
 const progressBarFull =document.querySelector('#progressBarFull');
+
+let currentQuestion = {};
+let acceptingAnswers = true;
+let score = 0; // Score is starting at zero
+let questionCounter = 0; // Question is startiing at zero
+let availableQuestions = []
+
+let questions = [
+    {
+        question: 'What is 2 + 2?',
+        choice1: '2',
+        choice2: '4',
+        choice3: '21',
+        choice4: '17',
+        answer: 4
+    },
+    {
+        question: 'What is 2 + 2?',
+        choice1: '2',
+        choice2: '4',
+        choice3: '21',
+        choice4: '17',
+        answer: 4
+    },
+    {
+        question: 'What is 2 + 2?',
+        choice1: '2',
+        choice2: '4',
+        choice3: '21',
+        choice4: '17',
+        answer: 4
+    },
+    {
+        question: 'What is 2 + 2?',
+        choice1: '2',
+        choice2: '4',
+        choice3: '21',
+        choice4: '17',
+        answer: 4
+    }
+]
