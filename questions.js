2e0a2189adbe2dde9125ad0143bf1001ceaba55a
rpc08002 <rcurtin0491@gmail.com>
2020-11-19 18:03:32 -0500

    added script for progression bar

--- questions.js
+++ questions.js
@@ -44,3 +44,13 @@ let questions = [
         answer: 4
     }
 ]
+
+const SCORE_POINTS = 100
+const MAX_QUESTIONS = 4
+
+startGame = () => {
+    questionCounter = 0;
+    score = 0;
+    availableQuestions = [...questions]
+    getNewQuestions()
+}
