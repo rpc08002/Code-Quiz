f2c214b8b3b5aa8d9c2ad76334e9371d278ff112
rpc08002 <rcurtin0491@gmail.com>
2020-11-20 15:52:23 -0500

    added variables, event listener, prevent default, and local storage for scores

---
+++ end.js
@@ -0,0 +1,35 @@
+const username = document.querySelector('#username');
+const saveScoreBtn = document.querySelector('#saveScoreBtn');
+const finalScore = document.querySelector('#finialScore');
+const mostRecentScore = document.querySelector('#mostRecentScore');
+
+const highScores = JSON.parse(localStorage.getItem('highScores')) || []
+
+const MAX_HIGH_SCORES = 5
+
+finalScore.InnerText = mostRecentScore
+
+// Will disable save button if no name is in field
+username.addEventListener('keyup', () => {
+    saveScoreBtn.disabled = !username.value
+})
+
+saveHighscore = e => {
+    e.preventDefault()
+
+    const score = {
+        score: mostRecentScore,
+        name: username.value
+    }
+
+    highScores.push(score)
+
+    highScores.sort((a,b) => {
+        return b.score - a.score
+    })
+
+    highScores.splice(5)
+
+    localStorage.setItem('highScores', JSON.stringify(highScores))
+    window.location.assign('/')
+}
